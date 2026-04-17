259f3590cdb04ed9cae9f56664cd64283600646a
Arksector <99473957+nulldrf@users.noreply.github.com>
2026-04-17 12:00:25 +0700

    3

--- lawnchair/src/app/lawnchair/settings/ui/LawnchairSettingsFragment.kt
+++ lawnchair/src/app/lawnchair/settings/ui/LawnchairSettingsFragment.kt
@@ -2,12 +2,32 @@ package app.lawnchair.settings.ui
 
 import android.os.Bundle
 import android.view.View
-import com.android.launcher3.settings.SettingsActivity.LauncherSettingsFragment
+import androidx.preference.PreferenceFragmentCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.android.launcher3.R
 
-class LawnchairSettingsFragment : LauncherSettingsFragment() {
+class LawnchairSettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.launcher_preferences, rootKey)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Critical: enable nested scrolling so AppBarLayout receives scroll events
         listView.isNestedScrollingEnabled = true
+
+        // Apply bottom inset padding for navigation bar
+        androidx.core.view.ViewCompat.setOnApplyWindowInsetsListener(listView) { v, insets ->
+            val systemBars = insets.getInsets(androidx.core.view.WindowInsetsCompat.Type.systemBars())
+            v.setPadding(
+                v.paddingLeft,
+                v.paddingTop,
+                v.paddingRight,
+                systemBars.bottom
+            )
+            insets
+        }
     }
 }

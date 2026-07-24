ae9cd72bbbed3fd9584ccb7aa6db3be53f3d9a82
Arksector <99473957+nulldrf@users.noreply.github.com>
2026-07-24 17:50:14 +0700

    Update DrawableTokens.kt

--- lawnchair/src/app/lawnchair/theme/drawable/DrawableTokens.kt
+++ lawnchair/src/app/lawnchair/theme/drawable/DrawableTokens.kt
@@ -7,6 +7,7 @@ import android.graphics.drawable.InsetDrawable
 import android.graphics.drawable.RippleDrawable
 import android.graphics.drawable.StateListDrawable
 import androidx.appcompat.content.res.AppCompatResources
+import app.lawnchair.preferences2.PreferenceManager2
 import app.lawnchair.theme.color.tokens.AllAppsTabColors
 import app.lawnchair.theme.color.tokens.ColorTokens
 import com.android.launcher3.R
